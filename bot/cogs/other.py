c5beada4d6e8c1fd0b3e93c88d83d24ab7691b77
Roman <prostopostacuba@gmail.com>
2023-02-23 00:08:22 +0200

    fixed bug for large aswers

--- bot/cogs/other.py
+++ bot/cogs/other.py
@@ -22,11 +22,11 @@ class __MainOtherCog(Cog):
         channel = self.bot.get_channel(1074413253893046362)
         embed = nextcord.Embed(title="Technical messages",
                                description=f'{self.bot.user.display_name}#{self.bot.user.discriminator} '
-                                f'was started at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
+                                           f'was started at {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
                                color=Colour.green())
         await channel.send(embed=embed)
         await self.bot.change_presence(status=Status.do_not_disturb,
-                                       activity=Activity(type=ActivityType.playing,name="In developing"))
+                                       activity=Activity(type=ActivityType.playing, name="In developing"))
         print(f'[LOGS]: {self.bot.user.display_name}#{self.bot.user.discriminator} was started at '
               f'{len(self.bot.guilds)} servers')
 
@@ -39,20 +39,34 @@ class __MainOtherCog(Cog):
         if not message.content.strip().startswith("!"):
             return
 
-        if len(message.content.strip())<10:
+        if len(message.content.strip()) < 10:
             return message.channel.send(content="`Мінімальна довжина запиту 10 символа`")
 
-        if message.channel.name == "🤖chatgpt🤖" :
+        if message.channel.name == "🤖chatgpt🤖":
             try:
-                promt = message.content[1::]
-                answer_message = await message.channel.send("`Очікуйте, бот генерує відповідь`")
-                response = await chatGPT.make_response(promt, message.author.id)
-                await answer_message.edit(content=response)
+                reply_message = await message.channel.send("`Очікуйте, бот генерує відповідь`")
+                prompt = message.content[1::]
+                test_message = ""
+                response = await chatGPT.make_response(prompt, message.author.id)
+                n = 2000
+                divided_messages = [response[i:i + n] for i in range(0, len(response), n)]
+                for i in divided_messages:
+                    embed = nextcord.Embed(title=self.bot.user.display_name,
+                                           description=f"`{i}`",
+                                           timestamp=datetime.utcnow(),
+                                           color=0x000000)
+                    embed.set_footer(text="Created by Romko")
+                    embed.set_author(name=message.author.display_name)
+                    await message.channel.send(embed=embed)
+                await reply_message.delete()
             except Exception as e:
-                embed = nextcord.Embed(title=f"Error occured", description=f"{str(e)}", color=0xff0000)
+                embed = nextcord.Embed(title=f"Error occured",
+                                       description=f"{str(e)}",
+                                       timestamp=datetime.utcnow(),
+                                       color=0xff0000)
+                embed.set_author(name=message.author.display_name)
                 embed.set_footer(text="Created by Romko")
                 await message.channel.send(embed=embed, content="")
-
 
     @Cog.listener()
     async def on_member_join(self, member):
